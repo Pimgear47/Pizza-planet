e4c01f1c0ef9924ec58eb3f035a4d5c23e70aef0
Kunjanaphorn Boonmak <kunjanaphorn.b@gmail.com>
2019-07-08 14:42:40 +0700

    shopping basket method

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,25 +1,142 @@
 <template>
-    <div class="row">
-        <div class="col-sm-12 col-md-6">
-            <table class="table table-hover">
-                <thead class="thead-default">
-                    <tr>
-                        <th>Size</th>
-                        <th>Price</th>
-                        <th>Add to basket</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td><strong>Margherita</strong></td>
-                    </tr>
-                    <tr>
-                        <td>9"</td>
-                        <td>12.95</td>
-                        <td><button class="btn btn-sm btn-outline-success" type="button">+</button></td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+  <div class="row">
+    <div class="col-xs-12 col-lg-6">
+      <table class="table table-hover">
+        <thead class="thead-default">
+          <tr>
+            <th>Size</th>
+            <th>Price</th>
+            <th>Add to basket</th>
+          </tr>
+        </thead>
+        <tbody v-for="item in getMenuItems" :key="item.name">
+          <tr>
+            <td>
+              <strong>{{item.name}}</strong>
+            </td>
+          </tr>
+          <tr v-for="option in item.options" :key="option.price">
+            <td>{{option.size}}</td>
+            <td>{{option.price}}</td>
+            <td>
+              <button
+                class="btn btn-sm btn-outline-success"
+                type="button"
+                @click="addToBasket(item,option)"
+              >+</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
+
+    <!-- Shopping basket -->
+    <div class="col-xs-12 col-lg-6">
+      <div v-if="basket.length > 0">
+        <table class="table">
+          <thead class="thead-default">
+            <tr>
+              <th>Quantity</th>
+              <th>Item</th>
+              <th>Total</th>
+            </tr>
+          </thead>
+          <tbody v-for="item in basket">
+            <tr>
+              <td>
+                <button class="btn btn-sm" type="button" @click="decreaseQuantity(item)">-</button>
+                <span>{{item.quantity}}</span>
+                <button class="btn btn-sm" type="button" @click="increaseQuantity(item)">+</button>
+              </td>
+              <td>{{item.name}} {{item.size}}"</td>
+              <td>{{item.price * item.quantity}}</td>
+            </tr>
+          </tbody>
+        </table>
+        <p>Order total:</p>
+        <button class="btn btn-success btn-block">Place order</button>
+      </div>
+      <div v-else>
+        <p>Your basket is empity!</p>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      basket: [],
+      getMenuItems: {
+        1: {
+          name: "Margherita",
+          description: "A delicious tomato based pizza topped with mozzarella",
+          options: [
+            {
+              size: 9,
+              price: 6.95
+            },
+            {
+              size: 12,
+              price: 10.95
+            }
+          ]
+        },
+        2: {
+          name: "Pepperoni",
+          description:
+            "A delicious tomato based pizza topped with mozzarella and pepperoni",
+          options: [
+            {
+              size: 9,
+              price: 7.95
+            },
+            {
+              size: 12,
+              price: 12.95
+            }
+          ]
+        },
+        3: {
+          name: "Ham and Pineapple",
+          description:
+            "A delicious tomato based pizza topped with mozzarella, ham and pineapple",
+          options: [
+            {
+              size: 9,
+              price: 7.95
+            },
+            {
+              size: 12,
+              price: 12.95
+            }
+          ]
+        }
+      }
+    };
+  },
+  methods: {
+    addToBasket(item, option) {
+      this.basket.push({
+        name: item.name,
+        price: option.price,
+        size: option.size,
+        quantity: 1
+      });
+    },
+    removeFromBasket(item) {
+      this.basket.splice(this.basket.indexOf(item), 1);
+    },
+    increaseQuantity(item) {
+      item.quantity++;
+    },
+    decreaseQuantity(item) {
+      item.quantity--;
+      if (item.quantity == 0) {
+        this.removeFromBasket(item);
+      }
+    }
+  }
+};
+</script>   
